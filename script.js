ddd95ad3c513ef38a30e102dd859dc206670e769
lums <jeremy@lums.io>
2020-10-25 18:17:11 +0100

    Add button event listeners & refactor

--- script.js
+++ script.js
@@ -1,23 +1,45 @@
-const easContainerDom = document.querySelector('#eas-container');
-//temporary place to put all the divs
-let d = document.createDocumentFragment();
-for (let index = 0; index < 16; index++) {
-    let square = document.createElement('div');
-    square.className = 'square';
-    square.innerHTML = "";
-    d.appendChild(square);
+const containerDom = document.querySelector('#eas-container');
+
+function makeSquares(num=32) {
+    //clear square grid
+    containerDom.textContent = '';
+    //temporary place to put all the divs
+    let d = document.createDocumentFragment();
+    for (let index = 0; index < num; index++) {
+        let square = document.createElement('div');
+        square.className = 'square';
+        square.style.backgroundColor = 'white';
+        square.innerHTML = "";
+        d.appendChild(square);
+    }
+    containerDom.style.gridTemplateColumns = `repeat(${ Math.sqrt(num)}, minmax(0px, 1fr))`;
+    containerDom.style.gridTemplateRows = `repeat(${Math.sqrt(num)}, minmax(0px,1fr))`;
+    containerDom.appendChild(d);
 }
-console.log(d);
-easContainerDom.appendChild(d);
+
+makeSquares(3);
 
 const squares = document.querySelectorAll('.square');
+const resetButton = document.querySelector('.l');
+const sizeButton = document.querySelector('.r');
+resetButton.addEventListener('click', () => {
+    squares.forEach((sqr) => {
+        sqr.style.backgroundColor = 'white';
+    });
+});
+
+sizeButton.addEventListener('click', () => {
+    const amount = prompt("how many squares?");
+    makeSquares(amount);
+});
 
 // iterate through each div
 squares.forEach((sqr) => {
 
-    // and for each one we add a 'click' listener
+    // hover function
+    //TODO: darken instead of black
     sqr.addEventListener('mouseover', () => {
-        console.log("i'm hovering!");
+        console.log(sqr.style.backgroundColor);
         sqr.style.backgroundColor = 'black';
         sqr.style.color = 'black';
     });
